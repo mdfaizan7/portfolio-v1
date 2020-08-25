441705322f653c8ede48e8368447be229f81f2ea
mdfaizan7 <faizaanazim@gmail.com>
2020-08-25 19:36:18 +0530

    completed the journey section

--- src/components/Journey/index.js
+++ src/components/Journey/index.js
@@ -1,85 +1,94 @@
-import React, { useState } from 'react'
-import VizSensor from 'react-visibility-sensor'
+import React, { useState } from "react"
+import VizSensor from "react-visibility-sensor"
 import {
-	VerticalTimeline,
-	VerticalTimelineElement,
-} from 'react-vertical-timeline-component'
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component"
 
-import Fade from '@material-ui/core/Fade'
+import Fade from "@material-ui/core/Fade"
+import StarIcon from "@material-ui/icons/Star"
 
-import Card from './ExpandingCard'
-import journeyAr from './journey'
+import Card from "./ExpandingCard"
+import journeyAr from "./journey"
 
-import 'react-vertical-timeline-component/style.min.css'
-import '../../styles/journey.scss'
+import "react-vertical-timeline-component/style.min.css"
+import "../../styles/journey.scss"
 
 const styles = {
-	leftEl: {
-		background: '#2a55a3',
-	},
-	leftAr: {
-		borderRight: '7px solid #2a55a3',
-	},
-	rightEl: {
-		background: '#c23c8e',
-	},
-	rightAr: {
-		borderRight: '7px solid #c23c8e',
-	},
+  leftEl: {
+    background: "#2a55a3",
+  },
+  leftAr: {
+    borderRight: "7px solid #2a55a3",
+  },
+  rightEl: {
+    background: "#c23c8e",
+  },
+  rightAr: {
+    borderRight: "7px solid #c23c8e",
+  },
 }
 
 function Journey() {
-	const [visible, setVisiblility] = useState(false)
+  const [visible, setVisiblility] = useState(false)
 
-	return (
-		<VizSensor
-			partialVisibility
-			onChange={isVisible => {
-				setVisiblility(isVisible)
-			}}
-		>
-			<div style={{ background: 'rgb(227, 227, 227)', overflow: 'hidden' }}>
-				<Fade in={visible} timeout={500}>
-					<div>
-						<VerticalTimeline>
-							{journeyAr &&
-								journeyAr.map((item, index) => (
-									<VerticalTimelineElement
-										contentStyle={
-											index % 2 === 0 ? styles.leftEl : styles.rightEl
-										}
-										contentArrowStyle={
-											index % 2 === 0 ? styles.leftAr : styles.rightAr
-										}
-										iconStyle={{
-											background: index % 2 === 0 ? '#2a55a3' : '#c23c8e',
-											color: '#fff',
-										}}
-										date={
-											window.innerWidth <= 700 ? (
-												<span style={{ color: '#fff' }}>{item.date}</span>
-											) : (
-												item.date
-											)
-										}
-										key={index}
-										icon={<item.icon />}
-									>
-										<Card
-											title={item.title}
-											subTitle={item.subTitle}
-											description={item.description}
-											detailsTitle={item.detailsTitle}
-											details={item.details}
-										/>
-									</VerticalTimelineElement>
-								))}
-						</VerticalTimeline>
-					</div>
-				</Fade>
-			</div>
-		</VizSensor>
-	)
+  return (
+    <VizSensor
+      partialVisibility
+      onChange={isVisible => {
+        setVisiblility(isVisible)
+      }}
+    >
+      <div style={{ background: "rgb(227, 227, 227)", overflow: "hidden" }}>
+        <Fade in={visible} timeout={1500}>
+          <div className="head-j">MY JOURNEY</div>
+        </Fade>
+        <Fade in={visible} timeout={500}>
+          <div style={{ marginBottom: 50 }}>
+            <div className="sub">Educational and Professional</div>
+            <VerticalTimeline>
+              {journeyAr &&
+                journeyAr.map((item, index) => (
+                  <VerticalTimelineElement
+                    contentStyle={
+                      index % 2 === 0 ? styles.leftEl : styles.rightEl
+                    }
+                    contentArrowStyle={
+                      index % 2 === 0 ? styles.leftAr : styles.rightAr
+                    }
+                    iconStyle={{
+                      background: index % 2 === 0 ? "#2a55a3" : "#c23c8e",
+                      color: "#fff",
+                    }}
+                    date={
+                      window.innerWidth <= 700 ? (
+                        <span style={{ color: "#fff" }}>{item.date}</span>
+                      ) : (
+                        item.date
+                      )
+                    }
+                    key={index}
+                    icon={<item.icon />}
+                  >
+                    <Card
+                      title={item.title}
+                      subTitle={item.subTitle}
+                      description={item.description}
+                      detailsTitle={item.detailsTitle}
+                      details={item.details}
+                    />
+                  </VerticalTimelineElement>
+                ))}
+              <VerticalTimelineElement
+                iconStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
+                icon={<StarIcon />}
+              />
+            </VerticalTimeline>
+          </div>
+        </Fade>
+      </div>
+    </VizSensor>
+  )
 }
 
 export default Journey
--- src/components/Journey/journey.js
+++ src/components/Journey/journey.js
@@ -1,118 +1,120 @@
-import SchoolIcon from '@material-ui/icons/School'
-import WorkIcon from '@material-ui/icons/Work'
+import SchoolIcon from "@material-ui/icons/School"
+import WorkIcon from "@material-ui/icons/Work"
 
 const journey = [
-	{
-		title: 'Finalists - Smart India Hackathon 2020',
-		subTitle: 'Government of India',
-		icon: WorkIcon,
-		date: 'August 2020',
-		description:
-			'Finalists of the Smart of India Hackathon conducted by Government of India. Finals were conducted on August 2020.',
-		details: [
-			'Proposed a solution to counter cyber-bulling and sexual harrasment in private chats of social media using Machine Learning techniques, without invading privacy of the user in any manner.',
-			'Various classical NLP techniques and Deep Learning techniques were used to find appropriate solution.',
-			'Made a DL Toxicity classifier to detect the toxicity of the message',
-			'Made a DL Predator Grooming Detector which would detect whether or not a person is a predator from a series of messages in a conversation',
-			'Made a chat app where we would implement these Deep Learning models.',
-		],
-	},
-	{
-		title: 'Full Stack Developer Intern',
-		subTitle: 'Spation',
-		icon: WorkIcon,
-		date: 'May 2020 - Present',
-		description:
-			'Spation is a Milan based startup which aims to turn restaurants into drop-in coworking spaces. It has a citywide network of distributed, flexible and affordable drop-in workspaces where you can discover your next favorite place to work hassle-free',
-		details: [
-			'Build a PWA using React',
-			'Worked on the backend and the frontend of their projects',
-			'Made a open-source donation platform for Italian restaurants',
-		],
-	},
-	{
-		title: 'JavaScript Algorithms and Data Structures',
-		subTitle: 'freeCodeCamp',
-		icon: SchoolIcon,
-		date: 'June 2020',
-		description:
-			'This certifiaction is given by freeCodeCamp which is a nonprofit organization. This certification roughly represents 300 hours of coursework on JavaScript Algorithms and Data Structures.',
-		details: [
-			'Basic JavaScript and ES6',
-			'Regular Expressions',
-			'Debugging',
-			'Basic Data Structures and Algorithm scripting',
-			'Object Oriented Programming and functional Programming',
-			'Intermediate Algorithm Scripting',
-		],
-	},
+  {
+    title: "Finalists - Smart India Hackathon 2020",
+    subTitle: "Government of India",
+    icon: WorkIcon,
+    date: "August 2020",
+    description:
+      "Finalists of the Smart of India Hackathon conducted by Government of India. Finals were conducted on August 2020.",
+    details: [
+      "Proposed a solution to counter cyber-bulling and sexual harrasment in private chats of social media using Machine Learning techniques, without invading privacy of the user in any manner.",
+      "Various classical NLP techniques and Deep Learning techniques were used to find appropriate solution.",
+      "Made a DL Toxicity classifier to detect the toxicity of the message",
+      "Made a DL Predator Grooming Detector which would detect whether or not a person is a predator from a series of messages in a conversation",
+      "Made a chat app where we would implement these Deep Learning models.",
+    ],
+  },
+  {
+    title: "Full Stack Developer Intern",
+    subTitle: "Spation",
+    icon: WorkIcon,
+    date: "May 2020 - Present",
+    description:
+      "Spation is a Milan based startup which aims to turn restaurants into drop-in coworking spaces. It has a citywide network of distributed, flexible and affordable drop-in workspaces where you can discover your next favorite place to work hassle-free",
+    details: [
+      "Build a PWA using React",
+      "Worked on the backend and the frontend of their projects",
+      "Made a open-source donation platform for Italian restaurants",
+      "Worked with Webpack and service-workers",
+      "Used technologies like Stripe and Mapbox",
+    ],
+  },
+  {
+    title: "JavaScript Algorithms and Data Structures",
+    subTitle: "freeCodeCamp",
+    icon: SchoolIcon,
+    date: "June 2020",
+    description:
+      "This certifiaction is given by freeCodeCamp which is a nonprofit organization. This certification roughly represents 300 hours of coursework on JavaScript Algorithms and Data Structures.",
+    details: [
+      "Basic JavaScript and ES6",
+      "Regular Expressions",
+      "Debugging",
+      "Basic Data Structures and Algorithm scripting",
+      "Object Oriented Programming and functional Programming",
+      "Intermediate Algorithm Scripting",
+    ],
+  },
 
-	{
-		title: 'APIs and Microservices Certification',
-		subTitle: 'freeCodeCamp',
-		icon: SchoolIcon,
-		date: 'May 2020',
-		description:
-			'This certifiaction is given by freeCodeCamp which is a nonprofit organization. This certification roughly represents 300 hours of coursework on API and Microservces.',
-		details: [
-			'Managing Packages with Npm',
-			'Basic Node and Express',
-			'MongoDB and Mongoose',
-			'Different Projects such as Timestamp Microservice, Request Header Parser, URL Shortner, Exercise Tracker and File Metadata Microservice.',
-		],
-	},
-	{
-		title: 'Modern React with Redux',
-		subTitle: 'Udemy, Stpethen Grider',
-		icon: SchoolIcon,
-		date: 'March 2020',
-		description:
-			'Modern React with Redux is a course taught by Stephen Grider on Udemy which focuses on the key aspects of React like webpack, hooks, etc.',
-		details: [
-			'React Lifecycle methods',
-			'JavaScript Module Systems',
-			'Making API Requests with React',
-			'State Management with Redux',
-			'Async Actions with Redux Thunk',
-			'Redux Routers',
-			'React Hooks',
-		],
-	},
-	{
-		subTitle: 'Effcon Technologies',
-		title: 'Summer Intern',
-		icon: WorkIcon,
-		date: 'June 2019 - July 2019',
-		description:
-			'Effcon Technologies is a team of techinical people. Was there as a Summer Intern focused on ML and Python.',
-		details: [
-			'Focused on Machine Learning Algorithms used for Regression and Classification.',
-			'Winner of two hackathons where I had to make a ML model for a given problem statement.',
-		],
-	},
-	{
-		title: 'BTech, Computer Science',
-		subTitle: 'Bhilai Institute of Technology, Durg',
-		icon: SchoolIcon,
-		date: 'August 2017 - Present',
-		description:
-			'CSE Undergraduate in Bachelors of Engineering from BIT Durg. It is a 4 year program, affiliated with AICTE.',
-		detailsTitle: 'Relevent Courses',
-		details: [
-			'Analysis and Design of Algorithms',
-			'Compiler Design',
-			'Computer Networks',
-			'Computer Systems Architecture',
-			'Database Management System',
-			'Data Structures',
-			'Inter-Networking with TCP-IP',
-			'Operating Systems',
-			'Software Engineering and Project Management',
-			'Theory of Computation',
-			'Unix and Shell Programming',
-			'Enterprise Resource Planning',
-		],
-	},
+  {
+    title: "APIs and Microservices Certification",
+    subTitle: "freeCodeCamp",
+    icon: SchoolIcon,
+    date: "May 2020",
+    description:
+      "This certifiaction is given by freeCodeCamp which is a nonprofit organization. This certification roughly represents 300 hours of coursework on API and Microservces.",
+    details: [
+      "Managing Packages with Npm",
+      "Basic Node and Express",
+      "MongoDB and Mongoose",
+      "Different Projects such as Timestamp Microservice, Request Header Parser, URL Shortner, Exercise Tracker and File Metadata Microservice.",
+    ],
+  },
+  {
+    title: "Modern React with Redux",
+    subTitle: "Udemy, Stpethen Grider",
+    icon: SchoolIcon,
+    date: "March 2020",
+    description:
+      "Modern React with Redux is a course taught by Stephen Grider on Udemy which focuses on the key aspects of React like webpack, hooks, etc.",
+    details: [
+      "React Lifecycle methods",
+      "JavaScript Module Systems",
+      "Making API Requests with React",
+      "State Management with Redux",
+      "Async Actions with Redux Thunk",
+      "Redux Routers",
+      "React Hooks",
+    ],
+  },
+  {
+    subTitle: "Effcon Technologies",
+    title: "Summer Intern",
+    icon: WorkIcon,
+    date: "June 2019 - July 2019",
+    description:
+      "Effcon Technologies is a team of techinical people. Was there as a Summer Intern focused on ML and Python.",
+    details: [
+      "Focused on Machine Learning Algorithms used for Regression and Classification.",
+      "Winner of two hackathons where I had to make a ML model for a given problem statement.",
+    ],
+  },
+  {
+    title: "BTech, Computer Science",
+    subTitle: "Bhilai Institute of Technology, Durg",
+    icon: SchoolIcon,
+    date: "August 2017 - Present",
+    description:
+      "CSE Undergraduate in Bachelors of Engineering from BIT Durg. It is a 4 year program, affiliated with AICTE.",
+    detailsTitle: "Relevent Courses",
+    details: [
+      "Analysis and Design of Algorithms",
+      "Compiler Design",
+      "Computer Networks",
+      "Computer Systems Architecture",
+      "Database Management System",
+      "Data Structures",
+      "Inter-Networking with TCP-IP",
+      "Operating Systems",
+      "Software Engineering and Project Management",
+      "Theory of Computation",
+      "Unix and Shell Programming",
+      "Enterprise Resource Planning",
+    ],
+  },
 ]
 
 export default journey
