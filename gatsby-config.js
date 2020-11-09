1be5af9b83887d9eb55f77e45c69709ac5b09788
mdfaizan7 <faizaanazim@gmail.com>
2020-11-10 00:09:55 +0530

    🔖 updated manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,8 +26,40 @@ module.exports = {
         description: "Faizan is a full stack dev with awesome skills 💯",
         background_color: `#fff`,
         theme_color: `#000000`,
-        display: `minimal-ui`,
-        icon: "src/images/icon.png",
+        icons: [
+          {
+            src: "icons/icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon.pmg",
+            sizes: "96x96 128x128",
+            type: "image/png",
+          },
+
+          {
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
       },
     },
     `gatsby-plugin-offline`,
